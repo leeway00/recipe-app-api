d1c5c0855986254fa94dcd3ad836c89d83060722
hun <lyh03057@snu.ac.kr>
2021-01-07 15:48:57 +0900

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
-# Create your models here.
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password= None, **extra_fields):
@@ -10,7 +9,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError("Users ust have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
-        #new usermodel 만드는 것)
+        #new usermodel 만드는 것
         user.set_password(password)
         user.save(using=self._db)
 
@@ -19,7 +18,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password):
         #creates and saves a new super user
         user=self.create_user(email, password)
-        user.is_staff =True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -30,8 +29,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     """custome user model that support susing email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default= True)
-    is_staff = models.BooleanField(default= False)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
 
     objects = UserManager()
 
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -19,7 +19,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.COM'
-        user =get_user_model().objects.create_user(email, 'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
@@ -30,10 +30,11 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """test creating a new superuser"""
-        user=get_user_model().objects.create_superuser(
+        user = get_user_model().objects.create_superuser(
             'test@londonappdev.com',
             'test123'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
